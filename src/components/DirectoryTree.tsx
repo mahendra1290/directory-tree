39076572ef34cdfcaba0ad1a9a14666e53e089b0
Mahendra Suthar <mahendra.suthar@goodworker.in>
2023-06-29 22:37:35 +0530

    fix vertical line

--- src/components/DirectoryTree.tsx
+++ src/components/DirectoryTree.tsx
@@ -1,6 +1,7 @@
 import Checkbox from './Checkbox';
 import dirIcon from '../assets/dirIcon.svg';
 import fileIcon from '../assets/fileIcon.svg';
+import { useEffect, useRef, useState } from 'react';
 
 interface DirectoryTreeProps {
   isRoot?: boolean;
@@ -38,9 +39,18 @@ const DirectoryTree = ({ fileNode, isRoot, onSelect, onDeselect }: DirectoryTree
     </span>
   );
 
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [bottom, setBottom] = useState(0);
+
+  useEffect(() => {
+    const lastChild = ref.current?.lastChild as HTMLDivElement | undefined;
+    setBottom((ref.current?.offsetHeight || 0) - (lastChild?.offsetTop || 0) + 14);
+  }, []);
+
   return (
     <div className='relative'>
-      <div className='absolute border bottom-3 top-6 left-1'></div>
+      <div style={{ bottom }} className='absolute border bottom-3 top-6 left-1'></div>
       {!isRoot && <div className='absolute right-full top-3 w-9 border'></div>}
       <Checkbox
         label={label}
@@ -49,7 +59,7 @@ const DirectoryTree = ({ fileNode, isRoot, onSelect, onDeselect }: DirectoryTree
         indeterminate={fileNode.indeteminate}
         className={fileNode.isDirectory ? '' : 'text-gray-500'}
       />
-      <div className='pl-10'>
+      <div ref={ref} className='pl-10'>
         {fileNode.children?.map((directory) => (
           <DirectoryTree
             key={directory.name}
