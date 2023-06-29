a4f22d1fe0120df00806a7cbcf80d01e799141c5
Mahendra Suthar <mahendra.suthar@goodworker.in>
2023-06-29 20:08:30 +0530

    add doc comment

--- src/components/DirectoryTree.tsx
+++ src/components/DirectoryTree.tsx
@@ -5,23 +5,21 @@ import fileIcon from '../assets/fileIcon.svg';
 interface DirectoryTreeProps {
   isRoot?: boolean;
   fileNode: FileNode;
-  allSelected?: boolean;
   onSelect?: (file: FileNode) => void;
   onDeselect?: (file: FileNode) => void;
 }
 
-const DirectoryTree = ({
-  fileNode: directory,
-  isRoot,
-  onSelect,
-  onDeselect,
-}: DirectoryTreeProps) => {
-  const handleFileChange = (file: FileNode) => (checked: boolean) => {
-    if (directory.indeteminate && !directory.selected) {
-      onSelect?.(file);
-      return;
-    }
-    if (checked) {
+/**
+ * DirectoryTree component that renders a directory tree recursively.
+ * @param props fileNode: the root node of the directory tree
+ * @param props isRoot: whether the current node is the root node
+ * @param props onSelect: callback function when a file is selected
+ * @param props onDeselect: callback function when a file is deselected
+ * @returns JSX.Element
+ */
+const DirectoryTree = ({ fileNode, isRoot, onSelect, onDeselect }: DirectoryTreeProps) => {
+  const handleSelectionChange = (file: FileNode) => (checked: boolean) => {
+    if (checked || fileNode.indeteminate) {
       onSelect?.(file);
     } else {
       onDeselect?.(file);
@@ -31,12 +29,12 @@ const DirectoryTree = ({
   const label = (
     <span className='flex font-mono items-center gap-2'>
       <img
-        src={directory.isDirectory ? dirIcon : fileIcon}
-        alt={directory.name}
+        src={fileNode.isDirectory ? dirIcon : fileIcon}
+        alt={fileNode.name}
         className='w-4 h-4'
       />
-      {directory.isDirectory && '/'}
-      {directory.name}
+      {fileNode.isDirectory && '/'}
+      {fileNode.name}
     </span>
   );
 
@@ -46,13 +44,13 @@ const DirectoryTree = ({
       {!isRoot && <div className='absolute right-full top-3 w-9 border'></div>}
       <Checkbox
         label={label}
-        checked={directory.selected}
-        onChange={handleFileChange(directory)}
-        indeterminate={directory.indeteminate}
-        className={directory.isDirectory ? '' : 'text-gray-500'}
+        checked={fileNode.selected}
+        onChange={handleSelectionChange(fileNode)}
+        indeterminate={fileNode.indeteminate}
+        className={fileNode.isDirectory ? '' : 'text-gray-500'}
       />
       <div className='pl-10'>
-        {directory.children?.map((directory) => (
+        {fileNode.children?.map((directory) => (
           <DirectoryTree
             key={directory.name}
             fileNode={directory}
